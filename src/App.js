cc74dd8efc4df52c7507b826c2a8afe767b10ab6
KPIWORKSTATION\Steve <steveyang9168@gmail.com>
2021-09-24 13:59:16 -0700

    add basic routing to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage= () => (
+  <div>
+     <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
-   <HomePage> </HomePage>
+    <div>
+      <switch>
+         <Route exact path='/' component={HomePage} />
+         <Route exact path='/hats' component={HatsPage} />
+      </switch>
+    </div>
+   
   );
 }
 
