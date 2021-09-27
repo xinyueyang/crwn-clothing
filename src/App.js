eb1785259577f8c3bed38cfba820880e54b61909
KPIWORKSTATION\Steve <steveyang9168@gmail.com>
2021-09-27 13:21:23 -0700

    Add collection-preview component and style

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
 
-const HatsPage= () => (
-  <div>
-     <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <switch>
          <Route exact path='/' component={HomePage} />
-         <Route exact path='/hats' component={HatsPage} />
+         <Route path='/shop' component={ShopPage} />
       </switch>
     </div>
    
