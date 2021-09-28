f46d13b7acc3d11405e7de2179f29cab37c9ef78
KPIWORKSTATION\Steve <steveyang9168@gmail.com>
2021-09-28 09:07:10 -0700

    Add FormInput and CustomButton components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './sign-in.style.scss';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
     constructor(props){
@@ -16,32 +18,35 @@ class SignIn extends React.Component {
         this.setState({email:'', password:''});
     }
      
-    handChange = event =>{
+    handleChange = event =>{
         const { value, name} = event.target;
         this.setState({[name]:value});
     }
 
     render(){
         return(
-            <div className ='sing-in'>
-                <h2>I ALREADY HAVE AN ACCOUNT</h2>
-                <span>SIGN IN WITH YOUR EMAIL AND PASSWORD</span>
+            <div className ='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}> 
-                    <input 
+                    <FormInput 
                        name='email' 
                        type='email' 
                        value={this.state.email} 
+                       label='Email'
                        required 
-                       onChange={this.handleChange} />
-                    <label>Email</label>
-                    <input 
+                       handleChange={this.handleChange} />
+                  
+                    <FormInput 
                        name='password' 
                        type='password' 
                        value={this.state.password} 
+                       label='password'
                        required 
-                       onChange={this.handleChange} />
-                    <label>Password</label>
+                       handleChange={this.handleChange} />
+                  <CustomButton type='submit'> Sign In </CustomButton>
+
                 </form>
             </div>
         )
