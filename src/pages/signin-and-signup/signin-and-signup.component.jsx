b9adf33bd993a20dee2ae7e0ee3d13afcf0ba21e
KPIWORKSTATION\Steve <steveyang9168@gmail.com>
2021-09-30 10:03:32 -0700

    Add Sign-up conponent for Firestore account creation and authentication

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import './signin-and-signup.style.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SigninAndSignupPage = () =>(
     <div className='sign-in-and-sign-up'>
         <SignIn></SignIn>
+        <SignUp></SignUp>
     </div>
 )
 
