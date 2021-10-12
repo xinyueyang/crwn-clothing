29863d68bbc75d7c48a9795aa2df27a6202d4929
KPIWORKSTATION\Steve <steveyang9168@gmail.com>
2021-10-12 11:18:57 -0700

    add persist store, persist reducer

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor} from './redux/store';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider} from 'react-redux';
-import store from './redux/store';
 
 ReactDOM.render(
   
   <Provider store = {store}>
     <BrowserRouter>
-      <App />
+     <PersistGate persistor={persistor}>
+         <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
 
